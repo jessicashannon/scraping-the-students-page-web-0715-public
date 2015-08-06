b8b5f476b2a4f4b07fd44c9528eac58aeec3b674
Adam Moelis <adam.moelis@gmail.com>
2015-08-06 12:58:57 -0400

    merge scraping

--- student-page-data.rb
+++ student-page-data.rb
@@ -3,42 +3,56 @@ require 'open-uri'  # => true
 require 'pry'       # => true
 
 class Student
-  attr_reader :name
+  attr_reader :name, :twitter, :linkedin, :github, :fourth_one
 
   def initialize(url)
     @url = url
     @html = open(@url).read  # ~> OpenURI::HTTPError: 404 Not Found
     @student_data = Nokogiri::HTML(@html)
     @name = @student_data.search("h4.ib_main_header").text
+    @twitter = @student_data.search("div.social-icons a").map {|link| link['href']}[0]
+    @linkedin= @student_data.search("div.social-icons a").map {|link| link['href']}[1]
+    @github = @student_data.search("div.social-icons a").map {|link| link['href']}[2]
+    @fourth_one = @student_data.search("div.social-icons a").map {|link| link['href']}[3]
+    @bio = @student_data.search("div.services p").first.text.strip
+    @education = @student_data.search("div.services ul li").text
+    @work = @student_data.search("div.services p")[1].text.strip
+    @tag_line = @student_data.search("div.textwidget h3").text
 
   end
 
 
 end
 
+def create_students(array)
+  array.each do |url|
+    name = Student.new(url)
+  end
+
+end
 
 
-html = open("http://web0715.students.flatironschool.com/students/adam_moelis.html").read  # ~> OpenURI::HTTPError: 404 Not Found
-student_data = Nokogiri::HTML(html)
-data_hash = {}
 
-data_hash[:social_media_array] = student_data.search("div.social-icons a").map {|link| link['href']} # twitter, linkedin, github, and the fourth one
+# data_hash[:social_media_array] = student_data.search("div.social-icons a").map {|link| link['href']} # twitter, linkedin, github, and the fourth one
 
-data_hash[:name] = student_data.search("h4.ib_main_header").text
+# data_hash[:name] = student_data.search("h4.ib_main_header").text
 
-data_hash[:bio] = student_data.search("div.services p").first.text.strip
+# data_hash[:bio] = student_data.search("div.services p").first.text.strip
 
-data_hash[:education] = student_data.search("div.services ul li").text
+# data_hash[:education] = student_data.search("div.services ul li").text
 
-#student_work_adam = student_data.search("div.services h4").text
+# #student_work_adam = student_data.search("div.services h4").text
 
-data_hash[:work] = student_data.search("div.services p")[1].text.strip # COME BACK TO TRY AND SELECT MORE THAN JUST ONE WORK EXPERIENCE
+# data_hash[:work] = student_data.search("div.services p")[1].text.strip # COME BACK TO TRY AND SELECT MORE THAN JUST ONE WORK EXPERIENCE
 
-data_hash[:tag_line] = student_data.search("div.textwidget h3").text
+# data_hash[:tag_line] = student_data.search("div.textwidget h3").text
 
 
 adam = Student.new("http://web0715.students.flatironschool.com/students/adam_moelis.html")
-puts adam.name
+puts adam.twitter
+puts adam.linkedin
+puts adam.github
+puts adam.fourth_one
 
 
 # ~> OpenURI::HTTPError
