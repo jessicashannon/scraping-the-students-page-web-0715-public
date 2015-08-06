56559a086f596c79773fe2382c5cc64ad3ebda5d
Adam Moelis <adam.moelis@gmail.com>
2015-08-06 15:35:07 -0400

    done

--- student-page-data.rb
+++ student-page-data.rb
@@ -34,50 +34,14 @@ def clean_url_array
     end
   end
   clean_url_array
-
 end
 
  def create_students
-    
     student_array = clean_url_array.map do |url|
       student_array = Student.new(url)
     end
 end
 
-
-
-# def generate_names 
-#   name_array = []
-#   clean_array.each do |url|
-#     html = open(url).read
-#     index_data = Nokogiri::HTML(html)
-#     name = index_data.search("h4.ib_main_header").text.downcase.gsub(" ","_")
-#     name_array << name 
-#   end
-#   name_array
-# end
-
-
-
-
-
-
-
-
-
-# data_hash[:social_media_array] = student_data.search("div.social-icons a").map {|link| link['href']} # twitter, linkedin, github, and the fourth one
-
-# data_hash[:name] = student_data.search("h4.ib_main_header").text
-
-# data_hash[:bio] = student_data.search("div.services p").first.text.strip
-
-# data_hash[:education] = student_data.search("div.services ul li").text
-
-# #student_work_adam = student_data.search("div.services h4").text
-
-# data_hash[:work] = student_data.search("div.services p")[1].text.strip # COME BACK TO TRY AND SELECT MORE THAN JUST ONE WORK EXPERIENCE
-
-# data_hash[:tag_line] = student_data.search("div.textwidget h3").text
 
 
 
